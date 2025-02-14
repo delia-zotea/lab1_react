172fc2d4abc3ef3eb57107e99c0564e19408dda0
Zotea Delia <deliazotea@gmail.com>
2025-02-14 21:09:14 +0200

    heyo, aici am main js cu calcule de gradi

---
+++ script/main.js
@@ -0,0 +1,7 @@
+import { adunare, scadere } from './mathUtils.js';
+
+const numar1 = 10;
+const numar2 = 5;
+
+console.log(`Suma: ${adunare(numar1, numar2)}`);
+console.log(`Diferența: ${scadere(numar1, numar2)}`);
